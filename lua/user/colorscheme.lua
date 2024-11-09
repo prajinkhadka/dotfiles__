a301b4acc867a94cfa96179ef79c7c9a92c1a43a
prajinkhadka <prawjeenkhadka@gmail.com>
2024-11-09 15:04:35 -0600

    Updated dotfiles

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,49 @@
+local colorscheme = "gruvbox"
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
+
+-- require("gruvbox").setup({
+--   undercurl = true,
+--   underline = true,
+--   bold = true,
+--   italic = true,
+--   strikethrough = true,
+--   invert_selection = false,
+--   invert_signs = false,
+--   invert_tabline = false,
+--   invert_intend_guides = false,
+--   inverse = true, -- invert background for search, diffs, statuslines and errors
+--   contrast = "", -- can be "hard", "soft" or empty string
+--   overrides = {},
+--   dim_inactive = false,
+--   transparent_mode = true,
+-- })
+--
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   vim.notify("colorscheme " .. colorscheme .. " not found!")
+--   return
+-- end
---
+++ lua/user/leetcode.lua
@@ -0,0 +1,7 @@
+require("leetcode").setup({
+    -- Your configuration options here
+    -- For example, to set the language to Python
+    lang = "python3",
+})
+
+
